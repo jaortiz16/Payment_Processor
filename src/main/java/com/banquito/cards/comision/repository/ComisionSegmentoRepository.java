2eb6d8cef1dc9a753916555822fb2dccb3566b54
Anthony Quishpe <aequishpe2@espe.edu.ec>
2025-01-05 20:35:29 -0500

    Update Comision SegmentoRepository
    Update Comision SegmentoRepository

--- src/main/java/com/banquito/cards/comision/repository/ComisionSegmentoRepository.java
+++ src/main/java/com/banquito/cards/comision/repository/ComisionSegmentoRepository.java
@@ -5,7 +5,6 @@ import com.banquito.cards.comision.model.ComisionPK;
 import com.banquito.cards.comision.model.ComisionSegmento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
