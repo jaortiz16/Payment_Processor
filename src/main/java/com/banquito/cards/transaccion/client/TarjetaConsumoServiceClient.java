66267c04733124b2513e13bc6871b1ca18feddd7
Andres Ortiz <63428778+jaortiz16@users.noreply.github.com>
2025-01-10 00:13:28 -0500

    Update TarjetaConsumoServiceClient.java

--- src/main/java/com/banquito/cards/transaccion/client/TarjetaConsumoServiceClient.java
+++ src/main/java/com/banquito/cards/transaccion/client/TarjetaConsumoServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "tarjetaConsumoService", url = "https://efe3-2803-6320-2a-33f0-6881-87c7-5db-3f37.ngrok-free.app")
+@FeignClient(name = "tarjetaConsumoService", url = "https://2f3a-2803-6320-2a-33f0-281e-df89-dad4-ff52.ngrok-free.app")
 public interface TarjetaConsumoServiceClient {
 
     @PostMapping("/transacciones/consumo-tarjeta")
